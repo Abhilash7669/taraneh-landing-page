1428e1ef1c7d2aa3446bad76c3d25f512a149744
Abhilash7669 <abhilashsk1998@gmail.com>
2025-09-07 21:42:07 +0530

    taraneh-lp 4 sections update

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const header = document.querySelector(".header");
+
+    const observer = new IntersectionObserver(() => {
+        
+    })
+    
+});
